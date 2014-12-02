c31cecec7ab686b719e1417951b128f997f7aa9c
Radu-Bogdan Croitoru <croitoruradubogdan@gmail.com>
2014-12-02 23:49:37 +0200

    Initialize repo and add skeleton

---
+++ lib/comparateur.rb
@@ -0,0 +1,17 @@
+#require "comparateur/version"
+
+module Le
+  class Comparateur
+
+    class << self
+      def initialize
+        1
+      end
+    end
+
+    def calculate_similarity
+      puts "Radu"    
+    end
+
+  end
+end
---
+++ lib/comparateur/version.rb
@@ -0,0 +1,3 @@
+module Comparateur
+  VERSION = "0.0.1"
+end
