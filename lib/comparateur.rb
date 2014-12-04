3cfa74082964ffdeb26dbf46f126fce97e705b7e
Radu-Bogdan Croitoru <croitoruradubogdan@gmail.com>
2014-12-05 00:51:54 +0200

    Implement passing of strings or Nokogiri objects

--- lib/comparateur.rb
+++ lib/comparateur.rb
@@ -6,11 +6,11 @@ module Le
   class Comparateur
 
     def calculate_similarity site1, site2
-      nok_site1 = Nokogiri::HTML(site1)
-      nok_site2 = Nokogiri::HTML(site2)
+      site1 = Nokogiri::HTML(site1) unless site1.is_a?(Nokogiri::HTML::Document)
+      site2 = Nokogiri::HTML(site2) unless site2.is_a?(Nokogiri::HTML::Document)
 
-      arr_site1 = nok_site1.search('*').map(&:name)
-      arr_site2 = nok_site2.search('*').map(&:name)
+      arr_site1 = site1.search('*').map(&:name)
+      arr_site2 = site2.search('*').map(&:name)
       lcs = Diff::LCS.LCS(arr_site1, arr_site2)
 
       return  (2.0 * lcs.length.to_f) / (arr_site1.length.to_f + arr_site2.length.to_f)
